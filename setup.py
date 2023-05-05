74244a4bc8d98d7555755c85edcc2a945131732d
AHMED ALI PATHAN <pathanahmedali786@gmail.com>
2023-05-06 00:11:11 +0530

    msg

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name="Project1",
+      version="0.0.1",
+      author='me',
+      author_email="pathanahmedali786@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"])
